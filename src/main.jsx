917894c534c81200bb957fec2d6ecac313a8f9a4
da4do0 <79230118+da4do0@users.noreply.github.com>
2024-06-06 17:46:05 +0200

    added hook for db session

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,35 @@
-import React from "react";
+import React, {  useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {SupabaseProvider} from './hooks/supabaseSession.hook';
 
 import Header from "./routes/header";
 import Main from "./routes/main/mainContainer";
 import User from "./routes/user/user";
 
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Header/>,
-    children:[{
-      index: true,
-      element: <Main/>
-    },{
-      path:"/user",
-      element: <User/>
-    }]
+    element: <Header />,
+    children: [
+      {
+        index: true,
+        element: <Main />,
+      },
+      {
+        path: "/user",
+        element: <User />,
+      },
+    ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <SupabaseProvider>
+      <RouterProvider router={router} />
+    </SupabaseProvider>
   </React.StrictMode>
 );
--- src/routes/user/user.jsx
+++ src/routes/user/user.jsx
@@ -1,27 +1,41 @@
-import React, {useState} from "react";
-import bcrypt from 'bcryptjs';
+import React, { useState } from "react";
+import {useSupabase} from '../../hooks/supabaseSession.hook';
 
-const User = ()=>{
-    const [username, username_set] = useState(null);
-    const [userInput, userInput_set] = useState("");
-    const [passInput, passInput_set] = useState("");
+const User = () => {
+  const [username, username_set] = useState(null);
+  const [userInput, userInput_set] = useState("");
+  const [passInput, passInput_set] = useState("");
 
-    const newUser = ()=>{
-        console.log(userInput, "userInput");
-        console.log(passInput, "passInput");
-        const newPass = bcrypt.hashSync(passInput, 10);
-        console.log(newPass);
-    }
+  const {supabase} = useSupabase();
 
-    
+  const newUser = async () => {
+    console.log(userInput, "userInput");
+    console.log(passInput, "passInput");
+    const { data, error } = await supabase
+      .from("Utenti")
+      .insert([{ nickname: "someValue", password: "otherValue" }])
+      .select();
+      console.log(error)
+      console.log(data)
+  };
 
+  return (
+    <>
+      <input type="text" onChange={(e) => userInput_set(e.target.value)} />
+      <input type="text" onChange={(e) => passInput_set(e.target.value)} />
+      <button onClick={newUser}>nuovo user</button>
+    </>
+  );
+};
+
+const Login = ()=>{
     return(
         <>
-            <input type="text" onChange={(e)=> userInput_set(e.target.value)}/>
-            <input type="text" onChange={(e)=> passInput_set(e.target.value)}/>
-            <button onClick={newUser}>nuovo user</button>
+            <form>
+                <input type="text" />
+            </form>
         </>
-    )
+    );
 }
 
 export default User;
